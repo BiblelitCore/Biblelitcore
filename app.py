91df2bff10715e01d5a6388f8f8eb6b7a6dadfed
James Whelan <whelanjames@Jamess-Air.lan>
2026-02-02 21:50:51 -0600

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -124,4 +124,6 @@ def quiz():
     '''.replace('{{ level }}', str(level)).replace('{{ question }}', question).replace('{{ area }}', area), options_list=options_list)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  import os
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
